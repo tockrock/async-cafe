51391ebfeb0cc84b7de1d16e20b51a4cfe2b7701
Takeshi Suzuki <tockrock@gmail.com>
2023-09-03 20:55:01 +0900

    Added starting point for Chapter01
    https://github.com/editorscut/ec013Async/

---
+++ Chapter01/00/WhosNext/Controllers/EntryVendors/AsyncEntryVendor.swift
@@ -0,0 +1,14 @@
+struct AsyncEntryVendor  {
+  func entry(for count: Int) -> Entry {
+    let imageName = imageName(for: count)
+    return Entry(imageName: imageName)
+  }
+}
+
+extension AsyncEntryVendor {
+  private func imageName(for int: Int) -> String {
+    let number = int % 51
+    return "\(number).circle"
+  }
+}
+
---
+++ Chapter01/00/WhosNext/Controllers/EntryVendors/SleepingEntryVendor.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct SleepingEntryVendor  {
+  func entry(for count: Int) -> Entry {
+    let imageName = imageName(for: count)
+    return Entry(imageName: imageName)
+  }
+}
+
+extension SleepingEntryVendor {
+  private func imageName(for int: Int) -> String {
+    let number = int % 51
+    return "\(number).circle"
+  }
+}
