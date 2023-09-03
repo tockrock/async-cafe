862baffb466bdda117f2ef672436bf8b81d7fd32
Takeshi Suzuki <tockrock@gmail.com>
2023-09-03 21:44:32 +0900

    throw

--- Chapter01/00/WhosNext/Controllers/EntryVendors/ErrorEntryVendor.swift
+++ Chapter01/00/WhosNext/Controllers/EntryVendors/ErrorEntryVendor.swift
@@ -7,6 +7,9 @@ struct ErrorEntryVendor  {
 
 extension ErrorEntryVendor {
   private func imageName(for int: Int) throws -> String {
+    if int.isMultiple(of: 5) {
+      throw MultipleOfFiveError(number: int)
+    }
     let number = int % 51
     return "\(number).circle"
   }
