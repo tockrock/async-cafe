68a7544f0532ad40a1bb4fd0467edac9bdf099fd
Takeshi Suzuki <tockrock@gmail.com>
2023-09-03 21:28:05 +0900

    Entry Controller

--- Chapter01/00/WhosNext/Controllers/EntryController.swift
+++ Chapter01/00/WhosNext/Controllers/EntryController.swift
@@ -1,9 +1,14 @@
 class EntryController {
   private var count = 0
+  private(set) var entries = [Entry]()
+  private let vendor = PlainEntryVendor()
 }
 
 extension EntryController {
   func next() {
     count += 1
+    let entriesCopy = entries
+    let newEntry = vendor.entry(for: count)
+    entries = entriesCopy + [newEntry]
   }
 }
