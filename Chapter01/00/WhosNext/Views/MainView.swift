7384e3e0b5a940ba25a7d8bbe622da6739ba3ee3
Takeshi Suzuki <tockrock@gmail.com>
2023-09-03 21:35:47 +0900

    The View

--- Chapter01/00/WhosNext/Views/MainView.swift
+++ Chapter01/00/WhosNext/Views/MainView.swift
@@ -1,13 +1,16 @@
 import SwiftUI
 
 struct MainView {
+  @StateObject private var controller = EntryController()
 }
 
 extension MainView: View {
   var body: some View {
     NavigationStack {
       VStack {
-        Text("View goes here")
+        EntryGrid(entries: controller.entries)
+        Button("Next",
+               action: controller.next)
       }
       .padding()
       .navigationTitle("Entries")
