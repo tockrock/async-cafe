f378504c7aa0c0dd48f7b8d25c85b7148b8241cf
Takeshi Suzuki <tockrock@gmail.com>
2023-09-08 20:56:19 +0900

    Async Function

--- Chapter01/00/WhosNext/Controllers/EntryVendors/AsyncEntryVendor.swift
+++ Chapter01/00/WhosNext/Controllers/EntryVendors/AsyncEntryVendor.swift
@@ -15,16 +15,21 @@ extension AsyncEntryVendor {
       throw MultipleOfFiveError(number: int)
     }
     let number = int % 51
-    return await number.description + suffix
+    return await number.description + suffix()
   }
 }
 
-extension AsyncEntryVendor {
-  private var suffix: String {
-    get async {
-      try? await Task.sleep(for:
-          .seconds(Int.random(in: 2...6)))
-      return ".circle"
-    }
-  }
+//extension AsyncEntryVendor {
+//  private var suffix: String {
+//    get async {
+//      try? await Task.sleep(for:
+//          .seconds(Int.random(in: 2...6)))
+//      return ".circle"
+//    }
+//  }
+//}
+
+func suffix() async -> String {
+  try? await Task.sleep(for: .seconds(Int.random(in: 2...6)))
+  return ".circle"
 }
