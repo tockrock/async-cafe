0f4ae7cba1ed0738fc9e980cb50c1b6c8ca4291f
Takeshi Suzuki <tockrock@gmail.com>
2023-09-08 20:47:40 +0900

    try await

--- Chapter01/00/WhosNext/Controllers/EntryVendors/AsyncEntryVendor.swift
+++ Chapter01/00/WhosNext/Controllers/EntryVendors/AsyncEntryVendor.swift
@@ -1,15 +1,19 @@
 struct AsyncEntryVendor  {
   func entry(for count: Int) async -> Entry {
-    let imageName = await imageName(for: count)
-    return Entry(imageName: imageName)
+    do {
+      let imageName = try await imageName(for: count)
+      return Entry(imageName: imageName)
+    } catch {
+      return errorEntry()
+    }
   }
 }
 
 extension AsyncEntryVendor {
   private func imageName(for int: Int) async throws -> String {
-    if int.isMultiple(of: 5 {
+    if int.isMultiple(of: 5) {
       throw MultipleOfFiveError(number: int)
-    })
+    }
     let number = int % 51
     try? await Task.sleep(for: .seconds(Int.random(in: 2...6)))
     return "\(number).circle"
