79b305068cc29cca8659f6d7e328f25677e85ecc
Takeshi Suzuki <tockrock@gmail.com>
2023-09-09 20:49:26 +0900

    Move Project

---
+++ Chapter01/WhosNextTests/WhosNextTests.swift
@@ -0,0 +1,42 @@
+//
+//  WhosNextTests.swift
+//  WhosNextTests
+//
+//  Created by Takeshi Suzuki on 2023/09/09.
+//
+
+import XCTest
+@testable import WhosNext
+
+final class WhosNextTests: XCTestCase {
+  let vendor = AsyncEntryVendor()
+  let entry3 = Entry(imageName: "3.circle")
+  
+  func testEntryCreation() async {
+    let result = await vendor.entry(for: 3)
+    XCTAssertEqual(result, entry3)
+  }
+  
+  func testErrorEntryCreation() async {
+    let result = await vendor.entry(for: 5)
+    XCTAssertEqual(result, errorEntry())
+  }
+  
+  func testImageNameCreatesEntry() async {
+    do {
+      let result = try await vendor.imageName(for: 3)
+      XCTAssertEqual(result, entry3.imageName)
+    } catch {
+      XCTFail(error.localizedDescription)
+    }
+  }
+  
+  func testImageNameCreatesError() async {
+    do {
+      let _ = try await vendor.imageName(for: 5)
+      XCTFail("Expected MultipleOfFiveError")
+    } catch {
+      XCTAssertEqual(error is MultipleOfFiveError, true)
+    }
+  }
+}
